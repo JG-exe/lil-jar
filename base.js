ef48d6be3ca3c27a92a64fce2506b9ee728e22fb
JG-exe <joachim.gautama@gmail.com>
2025-07-07 21:20:32 +0200

    v

--- base.js
+++ base.js
@@ -1,3 +1,23 @@
 // get day, render item of current day
 // figure out how to move back and forth between past items up till the current one
 // no peeking into the next ones
+
+let count = 0;
+const startDate = '2025-07-07';
+const start = new Date(startDate);
+
+init();
+
+function init(){
+    getCount();
+}
+
+function getCount(){
+    let d = new Date();
+    console.log(start, d);
+
+    const timeDiff = d - start;
+    count = 1 + timeDiff/(1000 * 3600 *24)
+
+    document.getElementById("nr").innerHTML = Math.floor(count);
+}
