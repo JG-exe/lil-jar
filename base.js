bb7dde859d6fd07f7984cf826d5a16c73c85e9db
JG-exe <joachim.gautama@gmail.com>
2025-07-08 22:36:09 +0200

    added reasons.js, restructure, added toDo

--- base.js
+++ base.js
@@ -1,11 +1,14 @@
+import Reasons from "./data/Reasons.js";
+
+let count = 0;
+const startDate = '2025-08-10';
+const start = new Date(startDate);
+let currentlyWatching = 0;
+
 // get day, render item of current day
 // figure out how to move back and forth between past items up till the current one
 // no peeking into the next ones
 
-let count = 0;
-const startDate = '2025-07-07';
-const start = new Date(startDate);
-let currentlyWatching = 0;
 
 init();
 
@@ -20,3 +23,16 @@ function getCount(){
     document.getElementById("nr").innerHTML = count;
 }
 // currentlyWatching > 0 => allow to go to next item
+function renderButtons(){
+    // if currentlyWatching == 0 => no next button
+    // uif currently watching item 0 => no previous button
+    // add rrandom button to pick between day 0 and current day
+}
+
+function addEventListeners(){
+    // add eventListeners to buttons
+}
+
+function rngItem(){
+    // pick random item to display
+}
---
+++ data/Reasons.js
@@ -0,0 +1,8 @@
+export const Reasons = [
+    "You are the most wonderful girl I've ever met.",
+    "You are amazing and wonderful and smart. And funny, but like really hilarious and sooo cute.",
+    "I really love your laugh",
+    "You give my world more colour",
+    "You are so amazingly cute about everything.",
+    "You choose me and made you mine and I fucking love it"
+]
