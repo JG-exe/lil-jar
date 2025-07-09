f767fb954e8b76dce1bec9797de9ea71edea0f3d
JG-exe <joachim.gautama@gmail.com>
2025-07-10 00:55:44 +0200

    adddedd reasons, updated logic

--- base.js
+++ base.js
@@ -1,14 +1,11 @@
 import Reasons from "./data/Reasons.js";
 
 let count = 0;
-const startDate = '2025-07-03'; //2025-08-10
+const startDate = '2025-07-01'; // 2025-08-10 doublecheck date before reveal
 const start = new Date(startDate);
 let currentlyWatching = 0;
 let slicedArr = []
 
-// get day, render item of current day
-// figure out how to move back and forth between past items up till the current one
-// no peeking into the next ones
 // change id="time" to fit message if it is one from the past?
 
 init();
@@ -29,6 +26,11 @@ function getData(){
 }
 
 function renderButtons(){
+    if (currentlyWatching == 0){
+        document.getElementById('prev').classList.add('dispNone');
+    } else {
+        document.getElementById('prev').classList.remove('dispNone');
+    }
     // if currentlyWatching == 0 => no next button
     // uif currently watching item 0 => no previous button
     // add rrandom button to pick between day 0 and current day
@@ -61,8 +63,9 @@ function rngItem(){
 
 function displayItem(i){
     console.log(currentlyWatching);
+    renderButtons();
     document.getElementById('reason').innerHTML = slicedArr[i];
     document.getElementById("nr").innerHTML = currentlyWatching + 1 + ` out of ${count} currently available reasons`;
 
-    document.getElementById('total').innerHTML = Reasons.length + 1;
+    document.getElementById('total').innerHTML = Reasons.length;
 }
