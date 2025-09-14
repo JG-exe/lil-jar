ab6f26a36822a1a046ffa121c9644cb5b9919a32
JG-exe <joachim.gautama@gmail.com>
2025-09-14 16:23:40 +0200

    added logic for offset instead of start date change, base for pinning done. restructured layout in css for future pinning system

--- base.js
+++ base.js
@@ -1,25 +1,32 @@
 import Reasons from "./data/Reasons.js";
 
+let offset = 1;
 let count = 0;
-const startDate = new Date(2025, 7, 9); 
+const startDate = new Date(2025, 7, 10); 
 const start = new Date(startDate);
 const rl = Reasons.length;
 let currentlyWatching = 0;
 let slicedArr = [];
+let pins =[];
 
 init();
 
 function init(){
+    if(!localStorage.getItem('pins')){
+    localStorage.setItem('pins', [0]);
+    }
+
     getData();
     checkNew();
     addEventListeners();
     displayItem(currentlyWatching);
+    pinAll()
 }
 
 function getData(){
     let d = new Date();
     const timeDiff = d - start;
-    currentlyWatching = Math.floor(timeDiff/(1000 * 3600 *24));
+    currentlyWatching = Math.floor(timeDiff/(1000 * 3600 *24)) + offset;
     count = currentlyWatching + 1;
     slicedArr = Reasons.slice(0, count);
 }
@@ -30,14 +37,54 @@ function renderButtons(){
     } else {
         document.getElementById('prev').classList.remove('dispNone');
     }
-        if (currentlyWatching == count -1){
+    if (currentlyWatching == count -1){
         document.getElementById('next').classList.add('dispNone');
     } else {
         document.getElementById('next').classList.remove('dispNone');
     }
+
+    if(pins.includes(currentlyWatching)){
+        document.getElementById('pin').classList.add('rot');
+    } else{
+        document.getElementById('pin').classList.remove('rot');
+    }
+
+    pinAll();
+}
+
+function pinAll(){
+    document.getElementById('innerPin').innerHTML = ''
+    pins.forEach(makePin);
+    //TODO finish pinning function with localstorage
+}
+
+function makePin(i){
+    document.getElementById('innerPin').innerHTML +=`<div   class="pinnedItem" id='${i}'>${i}</div>`;
+
+    document.getElementById(i).addEventListener('click', function(){
+        currentlyWatching = i;
+        displayItem(currentlyWatching);
+        })
 }
 
 function addEventListeners(){
+    document.getElementById('pin').addEventListener('click', function(){
+        if(pins.includes(currentlyWatching)){
+            pins = pins.filter(i => i !==currentlyWatching);
+            localStorage.setItem('pins', pins);
+            document.getElementById('pin').classList.remove('rot');
+            pinAll();
+
+        } else{
+            pins.push(currentlyWatching);
+            localStorage.setItem('pins', pins);
+            document.getElementById('pin').classList.add('rot');
+            pinAll();
+
+        }
+        console.log(pins);
+    })
+
     document.getElementById('prev').addEventListener('click', function(){
         if(currentlyWatching > 0){
             currentlyWatching -= 1;
@@ -68,6 +115,7 @@ function displayItem(i){
         document.getElementById('total').innerHTML = rl;
         
     }
+    pinAll();
 }
 
 function checkNew(){
